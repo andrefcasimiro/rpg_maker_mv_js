df3fe747c87d05e805a6c0d395c7adef659fd6eb
André Fernandes <andrefcasimiro@awayco.com>
2019-08-25 18:02:15 +0100

    Updated code. Left window with recipe-craftable-item now displaying.

--- Project1/js/plugins/CraftingSystem.js
+++ Project1/js/plugins/CraftingSystem.js
@@ -1,93 +1,122 @@
-(() => {
+(function() {
 
-  const parameters = PluginManager.parameters('CraftingSystem');
-  const Game_Interpreter_Plugin_Command = Game_Interpreter.prototype.pluginCommand;
+  var parameters = PluginManager.parameters('CraftingSystem');
 
-  GameInterpreter.prototype.pluginCommand = (command, args) => {
-
-    Game_Interpreter_Plugin_Command.call(this, command, args);
+  var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    _Game_Interpreter_pluginCommand.call(this, command, args);
 
     if (command === 'CraftingSystem') {
-      const action = args[0];
+      var action = args[0];
 
       switch (action) {
         case 'open':
-          SceneManager.push(Scene_CraftingSystem);
+          SceneManager.push(Scene_Crafting);
           break;
         default:
           return;
       }
     }
-  }
+  };
 
-  const Scene_CraftingSystem = () => {
+  function Scene_Crafting() {
     this.initialize.apply(this, arguments);
   }
 
-  Scene_CraftingSystem.prototype = Object.create(Scene_MenuBase.prototype);
-  Scene_CraftingSystem.prototype.constructor = Scene_CraftingSystem;
+  Scene_Crafting.prototype = Object.create(Scene_MenuBase.prototype);
+  Scene_Crafting.prototype.constructor = Scene_Crafting;
 
-  Scene_CraftingSystem.prototype.initialize = () => {
+  Scene_Crafting.prototype.initialize = function() {
     Scene_MenuBase.prototype.initialize.call(this);
   };
 
-  Scene_CraftingSystem.prototype.create = () => {
+  Scene_Crafting.prototype.create = function() {
     Scene_MenuBase.prototype.create.call(this);
-
-    // Index Window
-    this._indexWindow = new Window_CraftingSystemIndex(0, 0);
+    this._indexWindow = new Window_CraftingList(0, 0);
     this._indexWindow.setHandler('cancel', this.popScene.bind(this));
 
-    // Dimensions
-    const windowHeight = this._indexWindow.height;
-    const boxWidth = Graphics.boxWidth;
-    const boxHeight = Graphics.boxHeight - windowHeight;
-
-    // Status Window
-    this._statusWindow = new Window_CraftingSystemStatus(
-      0, windowHeight, boxWidth, boxHeight
-    );
-
+    var indexWindowHeight = this._indexWindow.height;
     this.addWindow(this._indexWindow);
-    this.addWindow(this._statusWindow);
-
-    this._indexWindow.setStatusWindow(this._statusWindow);
   };
 
-  const Window_CraftingSystemIndex = () => {
+  var Window_CraftingList = function() {
     this.initialize.apply(this, arguments);
   }
 
-  Window_CraftingSystemIndex.prototype = Object.create(
-    Window_Selectable.prototype
-  );
-  Window_CraftingSystemIndex.prototype.constructor = Window_CraftingSystemIndex;
+  Window_CraftingList.prototype = Object.create(Window_Selectable.prototype);
+  Window_CraftingList.prototype.constructor = Window_CraftingList;
 
-  Window_CraftingSystemIndex.lastTopRow = 0;
-  Window_CraftingSystemIndex.lastIndex = 0;
+  Window_CraftingList.lastTopRow = 0;
+  Window_CraftingList.lastIndex = 0;
 
-  Window_CraftingSystemIndex.prototype.initialize = (x, y) => {
-    const width = Graphics.boxWidth;
-    const height = this.fittingHeight(6); // arg - number of visible rows
-    Window_Selectable.prototype.initialize.call(
-      this, x, y, width, height
-    );
+  Window_CraftingList.prototype.initialize = function(x, y) {
+    var width = Graphics.boxWidth / 3;
+    var height = this.fittingHeight(10);
 
+    Window_Selectable.prototype.initialize.call(this, x, y, width, height);
     this.refresh();
-    this.setTopRow(Window_CraftingSystemIndex.lastTopRow);
-    this.select(Window_CraftingSystemIndex.lastIndex);
+    this.setTopRow(Window_CraftingList.lastTopRow);
+    this.select(Window_CraftingList.lastIndex);
     this.activate();
+  }
+
+  Window_CraftingList.prototype.maxCols = function() {
+    return 1;
   };
 
-  Window_CraftingSystemIndex.prototype.maximumColumns = () => {
-    return 3;
+  Window_CraftingList.prototype.maxItems = function() {
+    return this._recipesList ? this._recipesList.length : 0;
+  }
+
+  Window_CraftingList.prototype.update = function() {
+    Window_Selectable.prototype.update.call(this);
+    this.updateStatus();
   };
 
-  Window_CraftingSystemIndex.prototype.maximumItems = () => {
-    return this._list
-      ? this._list.length
-      : 0;
+  Window_CraftingList.prototype.updateStatus = function() {
+    if (this._statusWindow) {
+      console.log('index: ', this.index());
+    }
   };
 
+  // Maps all recipes in the player inventory
+  Window_CraftingList.prototype.refresh = function() {
+    this._recipesList = [];
+
+    $dataItems.forEach(dataItem => {
+      var note = dataItem && dataItem.note;
+
+      if (note) {
+        var noteJSON = JSON.parse(note);
+        var hasRecipe = Object.keys(noteJSON)[0] === 'recipe';
+
+        if (hasRecipe) {
+          var { recipe } = noteJSON;
+
+          // ID of the crafted item
+          var itemToCraft = recipe.creates;
+          var ingredients =  recipe.ingredients;
+
+          this._recipesList.push({
+            itemToCraft,
+            ingredients,
+          });
+        }
+      }
+    });
+
+    this.createContents();
+    this.drawAllItems();
+  };
+
+  // Draws each craftable item on the left-top window
+  Window_CraftingList.prototype.drawItem = function(index) {
+    var itemToCraftID = this._recipesList[index].itemToCraft;
+    var rect = this.itemRect(index);
+
+    var width = rect.width - this.textPadding();
+
+    this.drawItemName($dataItems[itemToCraftID], rect.x, rect.y, width);
+  };
 
 })();
