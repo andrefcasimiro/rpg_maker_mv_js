b02559b32826dc1469479cec57f528b6709a5637
André Fernandes <andrefcasimiro@awayco.com>
2019-08-31 18:15:08 +0100

    Indentation fixes

--- Project1/js/plugins/CharacterMovement.js
+++ Project1/js/plugins/CharacterMovement.js
@@ -1,63 +1,84 @@
+/*
+  <Character Movement>
+  v 0.1 - 31 August 2019
+
+  GIT_HUB:
+  https://github.com/andrefcasimiro/rpg_maker_mv_js/blob/master/Project1/js/plugins/CharacterMovement.js
+
+  DESCRIPTION:
+  Allows for switching between 3 different character sheets: Idle, Walk and Run, according to our movement state.
+
+  INSTRUCTIONS:
+  1. Copy and paste plugin.
+  2. Choose Idle, Walk and Run character sheets in the plugin's parameters window.
+  3. Run game.
+
+  PARAMS:
+  @param Actor_{ID}
+  ID - must be equal to the actor index in the database
+
+  @param {Pose}_{ID}
+  Pose - must be one of the following: 'Idle', 'Walk' or 'Run'
+  ID - can be anything as long as you don't repeate @param names
+*/
+
 /*:
 * @plugindesc Allow for player movement to cycle between 3 character sheets: idle, walk and run.
-* @author FBU <andrefcasimiro@gmail.com>
+* @author FBU <andrefcasimiro(at)gmail.com>
 *
-* // Right now, we cant use arrays in the plugin params, so if you wish to support more than an actor
-* // Copy and paste the following template.
-*
-* // INSTRUCTIONS:
-* // Actor_{ID} - ID must be a number
-* // Pose_Character_Sheet_{ID} - ID must be a string or a symbol.
-*
+* // ACTOR 1 --------------------------------
 * @param Actor_1
-* @param Idle_Character_Sheet_Actor1
+*
+* @param Idle_Actor1
 * @desc "The character set for idle movement"
 * @require 1
 * @dir img/characters/
 * @type file
 * @parent Actor_1
-*
-* @param Walking_Character_Sheet_Actor1
+
+* @param Walk_Actor1
 * @desc "The character set for walk movement"
 * @require 1
 * @dir img/characters/
 * @type file
 * @parent Actor_1
 *
-* @param Running_Character_Sheet_Actor1
+* @param Run_Actor1
 * @desc "The character set for running movement"
 * @require 1
 * @dir img/characters/
 * @type file
 * @parent Actor_1
+*
 */
 
 (function() {
-  var Parameters = PluginManager.parameters('CharacterMovement')
+  var Parameters = PluginManager.parameters('CharacterMovement');
 
-  var bank = []
-
+  var bank = [];
   Object.keys(Parameters).forEach((parameterKey, index) => {
-    var parameterKeyArray = parameterKey.split('_')
-    var lastIndex = parameterKeyArray && parameterKeyArray[parameterKeyArray.length - 1]
+    var parameterKeyArray = parameterKey.split('_');
+    var lastIndex = parameterKeyArray && parameterKeyArray[parameterKeyArray.length - 1];
 
-    var isID = parameterKeyArray[0] === 'Actor' && !isNaN(lastIndex)
-
+    var isID = parameterKeyArray[0] === 'Actor' && !isNaN(lastIndex);
     if (isID) {
       bank = bank.concat({
         actor: lastIndex,
         sheet: {},
-      })
+      });
     } else {
-      var key = parameterKeyArray
-      key.length = key.length - 1
-      key = key.join('_')
+      if (!Parameters[parameterKey]) {
+        return; 
+      }
+      var key = parameterKeyArray;
+      key.length = key.length - 1;
+      key = key.join('_');
 
       if (bank[bank.length - 1]) {
         bank[bank.length - 1].sheet = {
           ...bank[bank.length - 1].sheet,
           [key]: Parameters[parameterKey],
-        }
+        };
       }
     }
   })
@@ -65,12 +86,14 @@
   var actors = [];
   var isDashing, isWalking = false;
   var isStopped = true;
+  var action = 0;
+  var _cachedAction;
 
   var Scene_Map_Create = Scene_Map.prototype.create;
   Scene_Map.prototype.create = function() {
     Scene_Map_Create.call(this);
 
-    actors = bank.map(entry => $gameActors.actor(entry.actor))
+    actors = bank.map(entry => $gameActors.actor(entry.actor));
   };
 
   var Scene_Map_Update = Scene_Map.prototype.update;
@@ -82,9 +105,12 @@
 
   var setSheet = function(action) {
     actors.forEach(function (actor, index) {
-      actor.setCharacterImage(bank[index].sheet[action]),
-      actor.characterIndex()
-    })
+      if (!Object.keys(bank[index].sheet).length) {
+        return;
+      }
+
+      actor.setCharacterImage(bank[index].sheet[action], actor.characterIndex())
+    });
   }
 
   var managePlayerMovement = function() {
@@ -93,13 +119,23 @@
     isStopped = !isWalking && !isDashing;
 
     if (isStopped) {
-      setSheet("Idle_Character_Sheet");
+      action = 0;
     } else if (isDashing) {
-      setSheet("Running_Character_Sheet");
+      action = 1;
     } else if (isWalking) {
-      setSheet("Walking_Character_Sheet");
+      action = 2;
     }
 
-    $gamePlayer.refresh();
+    if (action !== _cachedAction) {
+      action === 0
+        ? setSheet('Idle')
+        : action === 2
+            ? setSheet('Walk')
+            : setSheet('Run');
+
+      $gamePlayer.refresh();
+    }
+
+    _cachedAction = action;
   }
 })();
